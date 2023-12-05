97d8cbba3c7d6d095011a30c8ea4cb98ba661066
cretaceousbob <151457002+cretaceousbob@users.noreply.github.com>
2023-12-05 13:35:17 -0500

    Update encryptiontest.ps1

--- encryptiontest.ps1
+++ encryptiontest.ps1
@@ -6,22 +6,23 @@ Function Show-Test{
     $testMessage = "Running " + $testTypes[$currentTest] + " Test #" + ($cycle + 1) + "..."
     $logMessage = "$date" + " $testMessage"
     Write-Host $testMessage
-    Add-Content -path C:\encryptionbenchmark\log.txt -value $logMessage
+    Add-Content -path $logPath -value $logMessage
 }
 
-$numberOfCycles = 2000
+$numberOfCycles = 2
 $cycles = 0..($numberOfCycles-1)
 $date = Get-Date
+$logDate = Get-Date -format "MM-dd-yy"
+$logDate = $logDate.Clone()
+$logPath = "C:\encryptionbenchmark\testlog-" + $logDate + ".txt"
 $compressAlgo = "uncompressed"
 $cipherAlgo = "AES","AES256","Camellia128","Camellia256","Blowfish","Twofish","3DES"
-Set-Clipboard $cycles
 $averageTimesArray = 0..(($cipherAlgo.Count * 2) + 1)
-$testTimesArray = 0..(($cipherAlgo.Count * 2) + 1)
+$testTimesArray = $averageTimesArray.Clone()
 foreach ($cell in $averageTimesArray){
-    $testTimesArray[$cell] = Get-Clipboard
+    $testTimesArray[$cell] = $cycles.Clone()
 }
-
-$testTypes = 0..(($cipherAlgo.Count * 2) + 1)
+$testTypes = $averageTimesArray.Clone()
 $testTypes[0] = "Non-Encrypted Container Pack"
 $testTypes[1] = "Non-Encrypted Container Unpack"
 $skip = 0
@@ -66,17 +67,19 @@ foreach ($cycle in $cycles){
 }
 Remove-Item c:\test -Recurse -Force
 
-$averageHeader = "$date" + " Average Time in Milliseconds:`n"
+$averageHeader = "$date " + "Average Time in Milliseconds:`n"
 Write-Host $averageHeader
-Add-Content -path C:\encryptionbenchmark\log.txt -value $averageHeader
+Add-Content -path $logPath -value $averageHeader
 $i = 0
 foreach ($array in $testTimesArray){
     $i += 0 
     $averageTimesArray[$i] = ($testTimesArray[$i].TotalMilliseconds | Measure-Object -Average).Average
-    $averageMessage = "$date " + $testTypes[$i] + ":"
-    Write-Host $averageMessage
+    $averageTypeHost = $testTypes[$i] + ":"
+    $averageTypeLog = "$date " + $testTypes[$i] + ":"
+    $averageLogTimes = "$date " + $averageTimesArray[$i]
+    Write-Host $averageTypeHost
     $averageTimesArray[$i]
-    Add-Content -path C:\encryptionbenchmark\log.txt -value $averageMessage
-    Add-Content -path C:\encryptionbenchmark\log.txt -value $averageTimesArray[$i]
+    Add-Content -path $logPath -value $averageTypeLog
+    Add-Content -path $logPath -value $averageLogTimes
     $i++
 }
